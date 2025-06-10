fe9c7fc9762064d06cc77d4d0781c710b7da5bee
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-06-10 22:54:54 +0530

    ''

--- tasks.lua
+++ tasks.lua
@@ -13,7 +13,7 @@ local function task2_2()
 end
 
 return {
-  deploy = 'luarocks build --local',
+  deploy = 'luarocks make --local',
   format = 'stylua .',
   ['local'] = task1,
   fn_with_args = function(arg1, arg2)
